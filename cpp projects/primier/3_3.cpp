776c88b6ac57c1fa111bc3adda90a42a195a29ce
Simon Li <simonli1215@outlook.com>
2018-08-07 15:43:56 +0800

    finish chapter 3

---
+++ cpp projects/primier/3_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+  using namespace std;
+  int chest = 42;
+  int waist = 0x42;
+  int inseam = 042;
+
+  cout << "Monsieur cuts a striking figure!\n";
+  cout << "chest = " << chest << "\n";
+  cout << "waist = " << waist << "\n";
+  cout << "inseam = " << inseam << "\n";
+  return 0;
+
+}
